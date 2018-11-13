9f411fbcafc2d14f13091edf2a5473b306cf87c6
Margaux Pellet <margauxpellet@MacBook-Air-de-Margaux.local>
2018-11-13 16:26:35 +0100

    creation de classe nom

--- pg220-5322/Name.java
+++ pg220-5322/Name.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.Date;
 
-public class Name{
+public class Name {
 
 	private int size = 10;
 
@@ -24,23 +24,30 @@ public class Name{
 	public give_size(){
 		return this.size = size;
 	}
-	public void setname(){
+	public void setname(String nom){
 		this.nom = nom ; 
 	}
 
-	public void setsizemax(){
+	public void setsizemax(int size){
 		this.size = size ; 
+	}
+
+	public String description() {
+		return "Point (" + this.nom + "," + this.size + ") " + super.description();
 	}
 
 	public static void main(String[] args) {
 	try {
 		Scanner sc = new Scanner (System.in);
+		String nom = args[4];
+
 		Date dat= new Date();
 		DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy") ;
 		String dt= dateFormat.format(dat) ;
-		System.out.println("Salut"nom ) ; 
+		System.out.println("Salut"+ nom.description() ) ; 
 		System.out.println(dt);
 
+		}
 	}
 }
 
