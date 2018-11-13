78852f8dd84a31576410817ed023490b02df9242
Margaux Pellet <margauxpellet@MacBook-Air-de-Margaux.local>
2018-11-13 15:34:57 +0100

    creation de classe

---
+++ pg220-5322/chatbot.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+import java.util.Date;
+
+public class Name{
+
+	private int size = 10;
+
+	private String nom;
+
+	public Name{
+		super();
+		this.nom = new Name();
+		this.size = new Name();
+	}
+
+	public Name(String nom, int size){
+		this.nom = nom;
+		this.size = size;
+	}
+
+	public void give_name(){
+		this.nom = nom;
+	}
+	public  give_size(){
+		return this.size = size;
+	}
+
+	public static void main(String[] args) {
+	try {
+		Scanner sc = new Scanner (System.in);
+
+	}
+}
+
